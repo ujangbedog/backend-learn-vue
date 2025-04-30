9f3b497170e1646b9cf44618d3a2571fbbe64156
ujangbedog <domba88galing@gmail.com>
2025-04-30 13:45:32 +0700

    fix: error huh

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config(); // ⬅ penting
+
 import { Request, ParamsDictionary, Response } from "express-serve-static-core";
 import { IncomingMessage, ServerResponse } from "http";
 import { ParsedQs } from "qs";
@@ -5,19 +8,16 @@ import app from "./app";
 
 const PORT = process.env.PORT || 3000;
 
-// Fungsi untuk menjalankan server
 const startServer = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
 };
 
-// Mengecek apakah aplikasi berjalan dalam mode pengembangan
 if (process.env.NODE_ENV === "development") {
   startServer();
 }
 
-// Ekspor handler untuk Vercel
 export default (
   req:
     | IncomingMessage
