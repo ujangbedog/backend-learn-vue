92236857488515598f01482ae91739bbd839fd0e
ujangbedog <domba88galing@gmail.com>
2025-04-30 12:47:14 +0700

    refactor: fix some error

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,19 @@ import app from "./app";
 
 const PORT = process.env.PORT || 3000;
 
-// Mengecek apakah aplikasi berjalan dalam mode pengembangan
-if (process.env.NODE_ENV === "development") {
+// Fungsi untuk menjalankan server
+const startServer = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
+};
+
+// Mengecek apakah aplikasi berjalan dalam mode pengembangan
+if (process.env.NODE_ENV === "development") {
+  startServer();
 }
 
 // Ekspor handler untuk Vercel
-export default app;
+export default (req, res) => {
+  app(req, res);
+};
