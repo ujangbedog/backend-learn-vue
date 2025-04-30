6f5d7329da5d8a23a6ec9d0a5399d36237c3f598
ujangbedog <domba88galing@gmail.com>
2025-04-30 11:24:12 +0700

    feat: test api hello from index

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/hello", (req, res) => {
+  res.status(200).json({ message: "Hello from Express + Bun!" });
+});
+
+// Port listening
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
