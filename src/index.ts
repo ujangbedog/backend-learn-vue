05d93a9598d7a3798976681d5419c381047881bb
ujangbedog <domba88galing@gmail.com>
2025-05-01 13:14:14 +0700

    chore: use vercel node

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import app from "../src/app";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default (req: any, res: any) => app(req, res);
+export default (req: VercelRequest, res: VercelResponse) => {
+  return app(req as any, res as any);
+};
