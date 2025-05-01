1146a972676d7af0584bd3bd548f257c9f7ef65e
ujangbedog <domba88galing@gmail.com>
2025-05-01 14:16:08 +0700

    chore: allow cors to *

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,14 @@ import errorMiddleware from "./middlewares/errorMiddleware";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(homeRoutes);
 app.use(helloRoutes);
