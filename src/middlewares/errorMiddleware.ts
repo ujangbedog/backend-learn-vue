f69f4fd7d056e145642d342a477a917250169276
ujangbedog <domba88galing@gmail.com>
2025-05-01 20:38:22 +0700

    chore: clean

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -1,13 +1,29 @@
 import { Request, Response, NextFunction } from "express";
 
+interface CustomError extends Error {
+  statusCode?: number;
+  details?: any;
+}
+
 const errorMiddleware = (
-  err: any,
+  err: CustomError,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
+  const statusCode = err.statusCode || 500;
+
+  console.error("Error:", {
+    message: err.message,
+    stack: err.stack,
+    details: err.details || null,
+  });
+
+  res.status(statusCode).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+    ...(err.details && { details: err.details }),
+  });
 };
 
 export default errorMiddleware;
